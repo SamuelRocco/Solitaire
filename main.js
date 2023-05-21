25a53ee00d62f43996d336255c46aafb48dfb4e8
SamuelRocco <samueljrocco@gmail.com>
2023-05-20 20:02:52 -0400

    waste and column1
    waste, stock, and column1 pile buttons work at getting item out of array

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ window.onload = pageload;
 const cardfiles = [];
 var DECK = [];
 var shuffledDeck;
-var facedownpile = [];
-var faceuppile = [];
+var stockpile = [];
+var wastepile = [];
 var spades = [];
 var hearts = [];
 var diamonds = [];
@@ -80,6 +80,7 @@ function pageload() {
   shuffledDeck = shuffle(DECK);
 
   dealCards();
+  createStockAndWaste();
   console.log(column1);
   console.log(column2);
   console.log(column3);
@@ -87,7 +88,7 @@ function pageload() {
   console.log(column5);
   console.log(column6);
   console.log(column7);
-  console.log(facedownpile);
+  console.log(stockpile);
 }
 
 
@@ -164,25 +165,251 @@ function dealCards() {
 
 
 
-  facedownpile = shuffledDeck;    //transfer all cards to the facedown pile
+  stockpile = shuffledDeck;    //transfer all cards to the facedown pile
 
-  facedownpile.forEach(element => {   //make every card face down in the facedown pile
+  stockpile.forEach(element => {   //make every card face down in the facedown pile
     element.facedown();
   });
 
   shuffledDeck = [];    //make shuffledDeck pile equal Null, not needed anymore, all cards are dealt
 
+
+  dealCardColumnPiles();
+
+}
+
+function createStockAndWaste() {
+  refreshwastepile();
+  refreshstockpile();
+  //stock pile
+  var image;
+  function refreshstockpile() {
+    if (stockpile.length != 0) {
+      image = "images/card_background/cardBackground.png";
+    } else {
+      image = "images/card_background/cardBlank.png";
+    }
+    var stock = document.getElementsByClassName("stock");
+    var stockpilebutton = document.createElement("button");
+    var cardimage = document.createElement("img");
+    cardimage.src = image;
+    stockpilebutton.appendChild(cardimage);
+    stockpilebutton.className += "stockcard";
+    stock[0].appendChild(stockpilebutton);
+    if (image == "images/card_background/cardBackground.png") {
+      stockpilebutton.onclick = stocktowaste;
+    } else if (image == "images/card_background/cardBlank.png") {
+      stockpilebutton.onclick = wastetostock;
+    }
+  }
+  
+  //waste pile
+  function refreshwastepile() {
+    if (wastepile.length > 0) {
+      var waste = document.getElementsByClassName("waste");
+      var wastepilebutton = document.createElement("button");
+      wastepilebutton.className += "stockcard";
+      var image2 = wastepile[wastepile.length - 1].image; //last image on top
+      var cardimage2 = document.createElement("img");
+      cardimage2.src = image2;
+      wastepilebutton.appendChild(cardimage2);
+      waste[0].appendChild(wastepilebutton);
+
+      wastepilebutton.addEventListener('click', function(){
+        var cardmoved = checkifcardgoesondifferentpile("wastepile");
+        if (cardmoved) {
+          var waste = document.getElementsByClassName("waste");
+          console.log(wastepile);
+          waste[0].textContent = "";
+          refreshwastepile();
+        }
+      }, false);
+    }
+  }
+
+  function wastetostock() {
+    stockpile = wastepile;
+    wastepile = [];
+    var waste = document.getElementsByClassName("waste");
+    waste[0].textContent = "";
+    var stock = document.getElementsByClassName("stock");
+    stock[0].textContent = "";
+    refreshstockpile();
+    refreshwastepile();
+  }
+
+  function stocktowaste() {
+    var shiftedstockcard = stockpile.shift();
+    shiftedstockcard.faceup();
+    wastepile.push(shiftedstockcard);
+    var waste = document.getElementsByClassName("waste");
+    waste[0].textContent = "";
+    var stock = document.getElementsByClassName("stock");
+    stock[0].textContent = "";
+    refreshstockpile();
+    refreshwastepile();
+  }
+}
+
+function dealCardColumnPiles() {
+  //for column 1
+  var c1 = document.getElementsByClassName("column1");
+  for (var a = 0; a < column1.length; a++) {
+    console.log("fuck");
+    var col1butt = document.createElement("button");
+    var image = column1[a].image;
+    var cardimage = document.createElement("img");
+    console.log(cardimage);
+    cardimage.src = image;
+    col1butt.appendChild(cardimage);
+    col1butt.className += "c1 r" + (a + 1).toString();
+    c1[0].appendChild(col1butt);
+
+//column1button***********************************************************************************************************************/
+
+    col1butt.addEventListener('click', function(){
+      var cardmoved = checkifcardgoesondifferentpile("column1");
+      if (cardmoved) {
+        c1[0].textContent = "";
+        console.log(column1);
+      }
+    }, false);
+  }
+
+
+//***********************************************************************************************************************/
+
+  //for column 2
+  var c2 = document.getElementsByClassName("column2");
+  for (var b = 0; b < column2.length; b++) {
+    var col2butt = document.createElement("button");
+    var image2 = column2[b].image;
+    var cardimage2 = document.createElement("img");
+    cardimage2.src = image2;
+    col2butt.appendChild(cardimage2);
+    col2butt.className += "c2 r" + (b + 1).toString();
+    c2[0].appendChild(col2butt);
+  }
+
+  //for column 3
+  var c3 = document.getElementsByClassName("column3");
+  for (var c = 0; c < column3.length; c++) {
+    var col3butt = document.createElement("button");
+    var image3 = column3[c].image;
+    var cardimage3 = document.createElement("img");
+    cardimage3.src = image3;
+    col3butt.appendChild(cardimage3);
+    col3butt.className += "c3 r" + (c + 1).toString();
+    c3[0].appendChild(col3butt);
+  }
+
+  //for column 3
+  var c4 = document.getElementsByClassName("column4");
+  for (var d = 0; d < column4.length; d++) {
+    var col4butt = document.createElement("button");
+    var image4 = column4[d].image;
+    var cardimage4 = document.createElement("img");
+    cardimage4.src = image4;
+    col4butt.appendChild(cardimage4);
+    col4butt.className += "c4 r" + (d + 1).toString();
+    c4[0].appendChild(col4butt);
+  }
+
+  //for column 3
+  var c5 = document.getElementsByClassName("column5");
+  for (var e = 0; e < column5.length; e++) {
+    var col5butt = document.createElement("button");
+    var image5 = column5[e].image;
+    var cardimage5 = document.createElement("img");
+    cardimage5.src = image5;
+    col5butt.appendChild(cardimage5);
+    col5butt.className += "c5 r" + (e + 1).toString();
+    c5[0].appendChild(col5butt);
+  }
+
+  //for column 3
+  var c6 = document.getElementsByClassName("column6");
+  for (var f = 0; f < column6.length; f++) {
+    var col6butt = document.createElement("button");
+    var image6 = column6[f].image;
+    var cardimage6 = document.createElement("img");
+    cardimage6.src = image6;
+    col6butt.appendChild(cardimage6);
+    col6butt.className += "c6 r" + (f + 1).toString();
+    c6[0].appendChild(col6butt);
+  }
+
+  //for column 3
+  var c7 = document.getElementsByClassName("column7");
+  for (var g = 0; g < column7.length; g++) {
+    var col7butt = document.createElement("button");
+    var image7 = column7[g].image;
+    var cardimage7 = document.createElement("img");
+    cardimage7.src = image7;
+    col7butt.appendChild(cardimage7);
+    col7butt.className += "c7 r" + (g + 1).toString();
+    c7[0].appendChild(col7butt);
+  }
+
 }
 
 
 
+//************************************************************************************* */
+
+
+function checkifcardgoesondifferentpile(arrayname) {
+
+  if (arrayname == "wastepile") {   //if coming from wastepile
+    console.log(arrayname);
+    wastepile.splice(-1, 1);     //its returning the 0th index for some reason
+    return true;
+  } else if (arrayname == "column1") {    //if coming from column1
+    console.log(arrayname);
+    column1.splice(-1,1);
+    return true;
+  } else if (arrayname == "column2") {    //if coming from column2
+    console.log(arrayname);
+  } else if (arrayname == "column3") {    //if coming from column3
+    console.log(arrayname);
+  } else if (arrayname == "column4") {    //if coming from column4
+    console.log(arrayname);
+  } else if (arrayname == "column5") {    //if coming from column5
+    console.log(arrayname);
+  } else if (arrayname == "column6") {    //if coming from column6
+    console.log(arrayname);
+  } else if (arrayname == "column7") {    //if coming from column7
+    console.log(arrayname);
+  } else {
+    return false;
+  }
+
+
+
+}
+
+//************************************************************************************* */
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+  // Please note that calling sort on an array will modify that array.
+  // you might want to clone your array first.
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
 class Card {
   constructor(card) {
     this.filename = card;
-    this.facevalue = card
-      .substring(card.indexOf("_") + 4)
-      .replace(/\.[^/.]+$/, "")
-      .replace(/[^a-zA-Z]+/g, "");
+    this.facevalue = card.substring(card.indexOf("_") + 4).replace(/\.[^/.]+$/, "").replace(/[^a-zA-Z]+/g, "");
     this.numvalue = this.findnumvalue(card);
     this.facingup = true;
     this.facingdown = false;
